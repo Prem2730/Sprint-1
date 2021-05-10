b4f3d35ebed0c5b028103e89258ec705115d25fd
Prem2730 <83799999+Prem2730@users.noreply.github.com>
2021-05-10 16:47:43 +0530

    Update Sprint1MovieTicketApplication.java

--- Sprint1MovieTicketApplication/src/main/java/com/capgemini/Sprint1MovieTicketApplication.java
+++ Sprint1MovieTicketApplication/src/main/java/com/capgemini/Sprint1MovieTicketApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Sprint1MovieTicketApplication {
-
+//this method is implemevugvyfgiouguy ratna
 	public static void main(String[] args) {
 		SpringApplication.run(Sprint1MovieTicketApplication.class, args);
 	}
